0bb9a18176887cb3ef221ca552c54022d86958bd
Robel-code-100 <robelabera509@gmail.com>
2025-09-08 18:49:22 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,25 @@
+# LoFAD - Log File Anomaly Detector
+# Package initialization file
+
+# Package metadata
+__version__ = "1.0.0"
+__author__ = "LoFAD Team"
+__license__ = "MIT"
+
+# Import main public API from internal modules
+from .detector import LoFAD
+from .alerts import send_email, send_slack, abuseipdb_check
+from .patterns import AUTH_PATTERNS, CMD_PATTERNS
+from .utils import RotatingTail
+
+# Define what is exposed when `from lofad import *` is used
+__all__ = [
+    "LoFAD",
+    "send_email",
+    "send_slack",
+    "abuseipdb_check",
+    "AUTH_PATTERNS",
+    "CMD_PATTERNS",
+    "RotatingTail",
+]
 
